accf4aec4868f9d8d367ce0272ef2dfd865d957e
vlrvdnkk <krasilnikovaaa21@st.ithub.ru>
2023-05-22 17:57:05 +0300

    Added new scenes and functional of buttons
    Added Levels scene + Lvl1, 2, 3 scenes, new functional of volume slider, added functional of levels menu button and levels button, music with DontDestroyOnLoad

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    
     void Awake()
     {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+
+        }
         DontDestroyOnLoad(this.gameObject);
     }
 }
